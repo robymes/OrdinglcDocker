fc30c6fb8e40bea48597084f34dd8be89f889c95
messorar <roberto.messora@valuelab.it>
2016-10-23 12:10:13 +0200

    refactoring

--- docker-swarm.sh
+++ docker-swarm.sh
@@ -1,2 +1,5 @@
 docker network create --driver overlay mainnetwork
 docker service create --name rabbitmq_bus --network mainnetwork --publish 15672:15672 rabbitmq:3.6.5-management
+docker service create --name mongo --network mainnetwork --publish 27017:27017 mongo:3.2
+docker service create --name node_ingestion --network mainnetwork --publish 8080:8080 robymes/node_ingestion:latest
+docker service create --name node_processing --network mainnetwork robymes/node_processing:latest
