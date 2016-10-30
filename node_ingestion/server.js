ab13e31f99821a16b6e138dfa247fe50a97a8c2e
messorar <roberto.messora@valuelab.it>
2016-10-30 10:46:09 +0100

    introduzione variabili d'ambiente

--- node_ingestion/server.js
+++ node_ingestion/server.js
@@ -3,10 +3,18 @@
 "use strict";
 
 var express,
+    util,
     bodyParser,
+    mongoHost,
+    mongoPort,
     mongoDb,
     mongoClient,
     mongoUrl,
+    itemsCollectionName = "items",
+    rabbitHost,
+    rabbitPort,
+    rabbitUrl,
+    rabbitQueue = "ordinglc",
     uuid,
     instanceUuid,
     app,
@@ -15,36 +23,40 @@ var express,
     insertDbItem,
     amqp,
     sendMessage,
-    itemsCollectionName = "items",
     countItemsByProcessedStatus;
 
 try {
     uuid = require("uuid");
     instanceUuid = uuid.v4();
+    util = require("util");
     express = require("express");
     bodyParser = require("body-parser");
     mongoDb = require("mongodb");
     amqp = require("amqplib/callback_api");
     mongoClient = mongoDb.MongoClient;
     app = express();
-    mongoUrl = "mongodb://mongo:27017/ordinglc";
+    mongoHost = process.env.ENV_MONGOHOST;
+    mongoPort = process.env.ENV_MONGOPORT;
+    mongoUrl = util.format("mongodb://%s:%s/ordinglc", mongoHost, mongoPort);
+    rabbitHost = process.env.ENV_RABBITHOST;
+    rabbitPort = process.env.ENV_RABBITPORT;
+    rabbitUrl = util.format("amqp://%s:%s", rabbitHost, rabbitPort);
     app.use(bodyParser.json());
 
     sendMessage = function (itemId, successCallback, errorCallback) {
-        amqp.connect("amqp://rabbitmq_bus", function (err, conn) {
+        amqp.connect(rabbitUrl, function (err, conn) {
             if (err) {
                 errorCallback(err);
             } else {
                 conn.createChannel(function (err, ch) {
-                    var q = "ordinglc";
                     if (err) {
                         errorCallback(err);
                     } else {
                         try {
-                            ch.assertQueue(q, {
+                            ch.assertQueue(rabbitQueue, {
                                 durable: false
                             });
-                            ch.sendToQueue(q, new Buffer.from(itemId));
+                            ch.sendToQueue(rabbitQueue, new Buffer.from(itemId));
                             console.log("Message sent with ID %s", itemId);
                             successCallback();
                         } catch (ex) {
